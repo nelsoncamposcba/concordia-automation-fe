bf78a44f25fef87bd42210fdde7b0b6f46f30f73
nelsoncamposcba <nelsoncamposcapa@gmail.com>
2023-11-09 14:25:55 -0300

    parcial clase_6

---
+++ src/test/java/clase_6/UnitTestCalculadora.java
@@ -0,0 +1,18 @@
+package clase_6;
+
+import org.testng.annotations.Test;
+
+public class UnitTestCalculadora {
+
+    @Test
+    public void testSumaCorrecta(){
+        Calculadora calculadora = new Calculadora();
+
+        int a = 10;
+        int b = 8;
+        int resultadoEsperado = 18;
+
+
+    }
+
+}
