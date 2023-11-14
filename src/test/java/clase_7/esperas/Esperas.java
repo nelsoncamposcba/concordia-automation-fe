3bcbdd6da6c23cd84e10d8e4d57e2b9e4b8a0dcd
nelsoncamposcba <nelsoncamposcapa@gmail.com>
2023-11-14 14:53:16 -0300

    Implicita

--- src/test/java/clase_7/esperas/Esperas.java
+++ src/test/java/clase_7/esperas/Esperas.java
@@ -1,11 +1,15 @@
 package clase_7.esperas;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class Esperas {
 
@@ -21,6 +25,9 @@ public class Esperas {
     public void testEsperaImplicita(){
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html");
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement landscape = driver.findElement(By.id("landscape"));
     }
 
     @AfterMethod
