383a1cc530beaab44420f1e8cb3b623061004a84
nelsoncamposcba <nelsoncamposcapa@gmail.com>
2023-11-14 15:23:04 -0300

    Espera Fluida

--- src/test/java/clase_7/esperas/Esperas.java
+++ src/test/java/clase_7/esperas/Esperas.java
@@ -2,10 +2,13 @@ package clase_7.esperas;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -37,6 +40,20 @@ public class Esperas {
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html");
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement landscape = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.id("landscape"))
+        );
+    }
+
+    @Test
+    public void testEsperaFluida(){
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html");
+
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(100))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+
         WebElement landscape = wait.until(
                 ExpectedConditions.presenceOfElementLocated(By.id("landscape"))
         );
